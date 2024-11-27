53e4ef4f3eb5fbb0ad3df925e4d035947de851d1
Maximilliano Cramer <maximillianocramer.dev@gmail.com>
2024-11-27 15:44:18 -0300

    atualização de qualidade

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sections = Array.from(document.querySelectorAll("section"));
-    const scrollArrow = document.getElementById("scrollArrow");
-  
-    let currentSectionIndex = 0;
-  
-    scrollArrow.addEventListener("click", () => {
-      currentSectionIndex++;
-      if (currentSectionIndex >= sections.length) {
-        currentSectionIndex = 0; // Volta ao topo
-      }
-  
-      const nextSection = sections[currentSectionIndex];
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    });
-  });
-  
+  const dialogBox1 = document.getElementById("dialogBox1"); // Primeiro balão
+  const dialogBox2 = document.getElementById("dialogBox2"); // Segundo balão
+  const dialogBox3 = document.getElementById("dialogBox3"); // Terceiro balão
+
+  // Exibe o primeiro balão imediatamente
+  dialogBox1.style.display = "block";
+  setTimeout(() => {
+    dialogBox1.style.display = "none"; // Esconde o primeiro balão após 4 segundos
+  }, 5000);
+
+  // Exibe o segundo balão após 5 segundos
+  setTimeout(() => {
+    dialogBox2.style.display = "block";
+    setTimeout(() => {
+      dialogBox2.style.display = "none"; // Esconde o segundo balão após 4 segundos
+    }, 5000);
+  }, 5000);
+
+  // Exibe o terceiro balão após 10 segundos
+  setTimeout(() => {
+    dialogBox3.style.display = "block";
+    setTimeout(() => {
+      dialogBox3.style.display = "none"; // Esconde o terceiro balão após 4 segundos
+    }, 5000);
+  }, 10000);
+});
